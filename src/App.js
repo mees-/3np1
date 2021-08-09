44171e934d9067314af621e329596c76086406ce
Mees van Dijk <mees@mees.io>
2021-08-09 23:36:50 +0200

    Add support for multiple lists

--- src/App.js
+++ src/App.js
@@ -1,106 +1,22 @@
-import { useMemo, useState } from "react"
-import { threeNPonePath } from "./lib"
+import { useReducer } from "react"
+import Card from "./Card"
 
 function App() {
-  const [inputNum, setInputNum] = useState(1)
-
-  const { path, longestEvenLength, sum, avg, max, min } = useMemo(() => {
-    const newPath = threeNPonePath(inputNum)
-    let longestEvenLength = 0
-    let currentEvenLength = 0
-    let sum = 0
-    let max = 0
-    let min = +Infinity
-    for (const num of newPath) {
-      sum += num
-      if (num > max) {
-        max = num
-      }
-      if (num < min) {
-        min = num
-      }
-      if (num % 2 === 0) {
-        currentEvenLength++
-        if (currentEvenLength > longestEvenLength) {
-          longestEvenLength = currentEvenLength
-        }
-      } else {
-        currentEvenLength = 0
-      }
-    }
-    const avg = sum / newPath.length
-    return { path: newPath, longestEvenLength, sum, avg, max, min }
-  }, [inputNum])
-
+  const [cards, addCard] = useReducer(prevState => [...prevState, <Card />], [<Card />])
   return (
     <div className="App">
-      <div className="card">
+      <div>
         <h1>3n+1</h1>
         <div className="explanation">
           <a href="https://en.wikipedia.org/wiki/Collatz_conjecture">Explanation</a>
+          <br />
+          <button onClick={addCard}>more lists</button>
         </div>
-        <input
-          type="number"
-          value={inputNum}
-          onChange={event => {
-            const value = parseInt(event.target.value)
-            setInputNum(value)
-          }}
-        ></input>
-        <button onClick={() => setInputNum(inputNum + 1)}>next</button>
-        <table>
-          <thead>
-            <tr>
-              <th>Stat</th>
-              <th>Value</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Average</td>
-              <td>{avg.toFixed(0)}</td>
-            </tr>
-            <tr>
-              <td>Max</td>
-              <td>{max}</td>
-            </tr>
-            <tr>
-              <td>Min</td>
-              <td>{min}</td>
-            </tr>
-            <tr>
-              <td>Sum</td>
-              <td>{sum}</td>
-            </tr>
-            <tr>
-              <td>consecutive evens</td>
-              <td>{longestEvenLength}</td>
-            </tr>
-          </tbody>
-        </table>
-        <table>
-          <thead>
-            <tr>
-              <th>Step</th>
-              <th>number</th>
-            </tr>
-          </thead>
-          <tbody>
-            {path !== null ? (
-              path.map((number, index) => (
-                <tr key={index}>
-                  <td>{index}</td>
-                  <td className={`num-${number % 2 === 0 ? "even" : "odd"}`}>{number}</td>
-                </tr>
-              ))
-            ) : (
-              <tr>
-                <td>0</td>
-                <td>{inputNum}</td>
-              </tr>
-            )}
-          </tbody>
-        </table>
+        <div className="list-container">
+          {cards.map((card, idx) => (
+            <div key={idx}>{card}</div>
+          ))}
+        </div>
       </div>
     </div>
   )
---
+++ src/Card.jsx
@@ -0,0 +1,100 @@
+import { useState, useMemo } from "react"
+import { threeNPonePath } from "./lib"
+
+export default function Card() {
+  const [inputNum, setInputNum] = useState(1)
+
+  const { path, longestEvenLength, sum, avg, max, min } = useMemo(() => {
+    const newPath = threeNPonePath(inputNum)
+    let longestEvenLength = 0
+    let currentEvenLength = 0
+    let sum = 0
+    let max = 0
+    let min = +Infinity
+    for (const num of newPath) {
+      sum += num
+      if (num > max) {
+        max = num
+      }
+      if (num < min) {
+        min = num
+      }
+      if (num % 2 === 0) {
+        currentEvenLength++
+        if (currentEvenLength > longestEvenLength) {
+          longestEvenLength = currentEvenLength
+        }
+      } else {
+        currentEvenLength = 0
+      }
+    }
+    const avg = sum / newPath.length
+    return { path: newPath, longestEvenLength, sum, avg, max, min }
+  }, [inputNum])
+  return (
+    <div className="card">
+      <input
+        type="number"
+        value={inputNum}
+        onChange={event => {
+          const value = parseInt(event.target.value)
+          setInputNum(value)
+        }}
+      ></input>
+      <button onClick={() => setInputNum(inputNum + 1)}>next</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Stat</th>
+            <th>Value</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Average</td>
+            <td>{avg.toFixed(0)}</td>
+          </tr>
+          <tr>
+            <td>Max</td>
+            <td>{max}</td>
+          </tr>
+          <tr>
+            <td>Min</td>
+            <td>{min}</td>
+          </tr>
+          <tr>
+            <td>Sum</td>
+            <td>{sum}</td>
+          </tr>
+          <tr>
+            <td>consecutive evens</td>
+            <td>{longestEvenLength}</td>
+          </tr>
+        </tbody>
+      </table>
+      <table>
+        <thead>
+          <tr>
+            <th>Step</th>
+            <th>number</th>
+          </tr>
+        </thead>
+        <tbody>
+          {path !== null ? (
+            path.map((number, index) => (
+              <tr key={index}>
+                <td>{index}</td>
+                <td className={`num-${number % 2 === 0 ? "even" : "odd"}`}>{number}</td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td>0</td>
+              <td>{inputNum}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
+  )
+}
