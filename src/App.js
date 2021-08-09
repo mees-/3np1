cbaabe1008be0fb95a22190c67f367167739ee7e
Mees van Dijk <mees@mees.io>
2021-08-09 22:15:46 +0200

    Add stats

--- src/App.js
+++ src/App.js
@@ -4,7 +4,33 @@ import { threeNPonePath } from "./lib"
 function App() {
   const [inputNum, setInputNum] = useState(1)
 
-  const path = useMemo(() => threeNPonePath(inputNum), [inputNum])
+  const { path, longestEvenLength, sum, avg, max, min } = useMemo(() => {
+    const newPath = threeNPonePath(inputNum)
+    let longestEvenLength = 0
+    let currentEvenLength = 0
+    let sum = 0
+    let max = 0
+    let min = +Infinity
+    for (const num of newPath) {
+      sum += num
+      if (num > max) {
+        max = num
+      }
+      if (num < min) {
+        min = num
+      }
+      if (num % 2 === 0) {
+        currentEvenLength++
+        if (currentEvenLength > longestEvenLength) {
+          longestEvenLength = currentEvenLength
+        }
+      } else {
+        currentEvenLength = 0
+      }
+    }
+    const avg = sum / newPath.length
+    return { path: newPath, longestEvenLength, sum, avg, max, min }
+  }, [inputNum])
 
   return (
     <div className="App">
@@ -22,7 +48,32 @@ function App() {
           }}
         ></input>
         <button onClick={() => setInputNum(inputNum + 1)}>next</button>
-
+        <table>
+          <thead>
+            <tr>
+              <th>Stat</th>
+              <th>Value</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Average</td>
+              <td>{avg.toFixed(0)}</td>
+            </tr>
+            <tr>
+              <td>Max</td>
+              <td>{max}</td>
+            </tr>
+            <tr>
+              <td>Min</td>
+              <td>{min}</td>
+            </tr>
+            <tr>
+              <td>consecutive evens</td>
+              <td>{longestEvenLength}</td>
+            </tr>
+          </tbody>
+        </table>
         <table>
           <thead>
             <tr>
